12f069e660def8331ce2920855bfbfc7840d6b9b
Victoria045 <victoriaberyl45@gmail.com>
2021-12-20 18:45:53 +0300

    create product form

--- order/forms.py
+++ order/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Customer
+from .models import Customer,Product
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
@@ -9,3 +9,8 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('image','name', 'price', 'digital',)
